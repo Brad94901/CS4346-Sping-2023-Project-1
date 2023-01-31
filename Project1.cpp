10bcc107655056aad292f0efde040160d556a58e
Brad94901 <brad94901@gmail.com>
2023-01-31 14:36:09 -0600

    Outline

--- Project1.cpp
+++ Project1.cpp
@@ -4,3 +4,8 @@ Brad Hughes, Zohair Khan
 
 Intelligent computer system design to diagnose toxi substances and poison, as well as recommend treatment based on the diagnosis.
 */
+
+int main(){
+
+	
+}
---
+++ Project1.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <string>
+#include <vector>
+
+#define PoisonVarListSize 50 //however many unique symptoms there are, ie nausea, hypertension, ect.
+#define PoisonConcListSize 30
