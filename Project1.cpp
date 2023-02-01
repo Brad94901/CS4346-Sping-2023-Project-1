40539f993d3eaee0a9731e464b93b18a87544572
Brad94901 <brad94901@gmail.com>
2023-02-01 16:37:24 -0600

    Function defs

--- Project1.cpp
+++ Project1.cpp
@@ -6,6 +6,9 @@ Intelligent computer system design to diagnose toxi substances and poison, as we
 */
 
 #include "Project1.h"
+#include <string>
+
+using namespace std;
 
 //Function declarations
 
@@ -13,26 +16,47 @@ int search_conclusion_list(string conclusion);
 int rule_to_clause(int rule);
 int upddate_VarList(int clause);
 int validate_Ri(int rule, string conclusion);
+string backwards_chain();
+void forwards_chain(string conclusion);
+
 
 int main(){
+
+	string conclusion = backwards_chain()
+	forwards_chain(conclusion);
 
 	
 }
 
+string backwards_chain(){ //driver function for backwards chain, returns the string conclusion.
+
+
+	return conclusion
+}
+
 int search_conclusion_list(string conclusion);{
+	string conclusion_list[POICONCLISTSIZE];
+	//populate the conclusion list
 
 }
 
 int rule_to_clause(int rule);{
-
 	int Ci = (rule - 1) * POISONVARLISTSIZE + 1;
 
 }
 
 int upddate_VarList(int clause);{
+	string VarList[POISONVARLISTSIZE] //maximum number of nodes a path can be +1 for a buffer
+	//populate based on path
+	//instantiate variables when needed
 
 }
 
 int validate_Ri(int rule, string conclusion);{
+	//check clauses and return conclusion
+
+}
+
+void forwards_chain(){ //driver fuctions for forwards chaing, returns void as our treatment plan will be displayed as part of the forwards chain.
 
 }
--- Project1.h
+++ Project1.h
@@ -4,5 +4,6 @@
 #include <string>
 #include <vector>
 
-#define POISONVARLISTSIZE 8 //however many unique symptoms there are, ie nausea, hypertension, ect.
-#define POISONCONCLISTSIZE 31
+#define POISONVARLISTSIZE 9 
+#define POISONS 31
+#define POICONCLISTSIZE 15//however many unique symptoms there are, ie nausea, hypertension, ect. 15 as a placeholder
